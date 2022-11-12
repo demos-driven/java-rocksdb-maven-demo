8b10b670fbf18a8d458a186d618703798015be69
Litong Deng <litong.deng@compass.com>
2022-11-12 15:24:22 +0800

    open rocksdb
    Signed-off-by: Litong Deng <litong.deng@compass.com>

--- src/main/java/Application.java
+++ src/main/java/Application.java
@@ -1,9 +1,28 @@
+import org.rocksdb.Options;
+import org.rocksdb.RocksDB;
+import org.rocksdb.RocksDBException;
+
 /**
  * Copyright (C) 2022 Urban Compass, Inc.
  */
 
 public class Application {
+  private static RocksDB openRocksDB() {
+    // loads the RocksDB C++ library
+    RocksDB.loadLibrary();
+
+    try (final Options options = new Options().setCreateIfMissing(true)) {
+      try (final RocksDB rocksDB = RocksDB.open(options, "./rocksdb")) {
+        System.out.println("Open rocksdb successfully.");
+        return rocksDB;
+      }
+    } catch (RocksDBException e) {
+      System.out.println("open rocksdb error: " + e);
+      return null;
+    }
+  }
+
   public static void main(String[] args) {
-    System.out.println("hello, world");
+    final RocksDB rocksDB = openRocksDB();
   }
 }
